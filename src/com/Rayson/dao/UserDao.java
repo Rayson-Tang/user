fb73c4acfbbe0e9bf61aa3f28f3237ace514500e
tmr620 <535704@qq.com>
2021-01-25 09:39:57 +0800

    Create UserDao.java

---
+++ src/com/Rayson/dao/UserDao.java
@@ -0,0 +1,46 @@
+package com.Rayson.dao;
+
+import com.Rayson.domain.User;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UserDao {
+    public List<User> findAll();
+
+
+
+    public  void add(User user);
+
+
+
+    public void update(User user);
+
+
+
+    public void delete(int id);
+
+
+
+    public User findById(int id);
+
+
+
+    public User findUserByUserNameAndPassword(String username, String password);
+
+    /**
+
+     * 查询总记录数
+
+     */
+
+    int findTotalCount( Map<String, String[]> condition);
+
+    /**
+
+     * 分页查询每页记录
+
+     */
+
+    List<User> findByPage(int start, int rows, Map<String, String[]> condition);
+}
